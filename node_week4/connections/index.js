76850099ef0d2a01e96d7a76d307088c73107f56
chukuohua <enjoy12589@gmail.com>
2023-04-01 22:00:03 +0800

    資料庫連接調整

--- node_week4/connections/index.js
+++ node_week4/connections/index.js
@@ -2,15 +2,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'})
 
-let DB = ''
-if (process.env.NODE_ENV === 'development') {
-  DB = 'mongodb://127.0.0.1:27017/hotel';
-} else {
-  DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-  )
-}
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
 
 // 連接資料庫
 mongoose.connect(DB)
