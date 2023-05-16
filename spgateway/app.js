3f7b4a6e0c71842787829c42f91def680157f1a1
ChuKuoHua <enjoy12589@gmail.com>
2023-05-16 17:20:03 +0800

    藍新測試

---
+++ spgateway/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const newebpayRouter = require('./routes/newebpay');
+const { resAllError } = require('./middleware/resError');
+const notFound = require('./service/notFound');
+const app = express();
+
+require('./connections');
+require('./middleware/processError');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', newebpayRouter);
+
+// 檢查路由
+app.use(notFound);
+
+// 錯誤處理
+app.use(resAllError);
+
+
+module.exports = app;
