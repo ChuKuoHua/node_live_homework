ebdb0109d202780f7ae86834af88dc6b64abfcee
ChuKuoHua <enjoy12589@gmail.com>
2023-05-24 11:31:54 +0800

    移除其他檔案

--- node_week2_typescript/bin/www.ts
+++
@@ -1,5 +0,0 @@
-import http from 'http';
-import app from '../app';
-const server: http.Server = http.createServer(app);
-const port = process.env.PORT || '3005';
-server.listen(port);
--- node_week5_ts/service/successHandle.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-function successHandle(res: Response, data: string[]) {
-  res.status(200).send({
-    "status": "success",
-    data
-  });
-}
-
-module.exports = successHandle
